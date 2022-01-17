330c9cf4c6433191cbccc18326a3a4cdae9faa1a
Mehak Gupta <55916817+mehakkkk@users.noreply.github.com>
2022-01-17 19:12:29 +0530

    Time: 2 ms (28.54%), Space: 40.7 MB (23.21%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,42 +1,17 @@
 class Solution {
-//     recursive approach
-        
-//         public int recursive(int l,int[] cost)
-//     {
-//         if(l >= cost.length)
-//             return 0;
-        
-//         //one-step
-//         int one = cost[l]+recursive(l+1,cost);
-//         //two-step
-//         int two = cost[l]+recursive(l+2,cost);
-        
-//         return Math.min(one,two);
-//     }
-//     public int minCostClimbingStairs(int[] cost) {
-//         return Math.min(recursive(0,cost),recursive(1,cost));
-//     }
-    
-    //recursion + memoization
-    int[] dp;
-    public int recursive(int l,int[] cost)
-    {
-        if(l >= cost.length)
-            return 0;
-        
-        if(dp[l]!=-1)
-            return dp[l];
-        
-        //one-step
-        int one = cost[l]+recursive(l+1,cost);
-        //two-step
-        int two = cost[l]+recursive(l+2,cost);
-        
-        return dp[l] = Math.min(one,two);
-    }
+    //using bottom up approach
     public int minCostClimbingStairs(int[] cost) {
-        dp = new int[cost.length+1];
-        Arrays.fill(dp,-1);
-        return Math.min(recursive(0,cost),recursive(1,cost));
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2;i<cost.length;i++)
+        {
+            int one = cost[i]+dp[i-1];
+            int two = cost[i]+dp[i-2];
+            
+            dp[i] = Math.min(one,two);
+        }
+        return Math.min(dp[cost.length-1],dp[cost.length-2]);
     }
 }
