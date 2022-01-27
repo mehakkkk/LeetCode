00ec6d24b622b97c9b4e0124ec860f46a05174a1
Mehak Gupta <55916817+mehakkkk@users.noreply.github.com>
2022-01-27 20:05:03 +0530

    Time: 16 ms (5.42%), Space: 133.6 MB (5.19%) - LeetHub

---
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int traverseRow(int start,int dest,int[] cost)
+    {
+        if(start == dest || start<0 || start>=cost.length)
+            return 0;
+        
+        if(start<dest) //move down
+        {
+            start += 1;
+            return cost[start] + traverseRow(start,dest,cost);
+        } 
+        else  //move up
+        {
+            start -= 1;
+            return cost[start] + traverseRow(start,dest,cost);
+        }
+    }
+    
+    public int traverseCol(int start,int dest,int[] cost)
+    {
+        if(start == dest || start<0 || start>=cost.length)
+            return 0;
+        
+        if(start<dest) //move right
+        {
+            start += 1;
+            return cost[start] + traverseCol(start,dest,cost);
+        } 
+        else  //move left
+        {
+            start -= 1;
+            return cost[start] + traverseCol(start,dest,cost);
+        }
+    }
+    public int minCost(int[] startPos, int[] homePos, int[] rowCosts, int[] colCosts) {
+        
+        int startX = startPos[0];
+        int startY = startPos[1];
+        
+        
+        int destX = homePos[0];
+        int destY = homePos[1];
+        
+        
+        int row = traverseRow(startX,destX,rowCosts);
+        int col = traverseCol(startY,destY,colCosts);
+        
+        return row+col;
+        
+    }
+}
