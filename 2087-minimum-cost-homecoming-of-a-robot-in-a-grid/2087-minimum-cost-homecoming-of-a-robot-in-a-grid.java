bcf3a4e66c65440fd82dc1cda01c071f5e69561c
Mehak Gupta <55916817+mehakkkk@users.noreply.github.com>
2022-01-27 20:17:15 +0530

    Time: 16 ms (5.42%), Space: 65.7 MB (39.97%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.java
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.java
@@ -1,18 +1,22 @@
 class Solution {
+    
+    public int[] rowDp,colDp;
     public int traverseRow(int start,int dest,int[] cost)
     {
         if(start == dest || start<0 || start>=cost.length)
             return 0;
+        if(rowDp[start] != 0)
+            return rowDp[start];
         
         if(start<dest) //move down
         {
             start += 1;
-            return cost[start] + traverseRow(start,dest,cost);
+            return rowDp[start] = cost[start] + traverseRow(start,dest,cost);
         } 
         else  //move up
         {
             start -= 1;
-            return cost[start] + traverseRow(start,dest,cost);
+            return rowDp[start] = cost[start] + traverseRow(start,dest,cost);
         }
     }
     
@@ -21,18 +25,25 @@ class Solution {
         if(start == dest || start<0 || start>=cost.length)
             return 0;
         
+        if(colDp[start] != 0)
+            return colDp[start];
+        
         if(start<dest) //move right
         {
             start += 1;
-            return cost[start] + traverseCol(start,dest,cost);
+            return colDp[start] = cost[start] + traverseCol(start,dest,cost);
         } 
         else  //move left
         {
             start -= 1;
-            return cost[start] + traverseCol(start,dest,cost);
+            return colDp[start] = cost[start] + traverseCol(start,dest,cost);
         }
     }
     public int minCost(int[] startPos, int[] homePos, int[] rowCosts, int[] colCosts) {
+        
+        rowDp = new int[rowCosts.length];
+        colDp = new int[colCosts.length];
+        
         
         int startX = startPos[0];
         int startY = startPos[1];
