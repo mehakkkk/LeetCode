a5539f3e5f85fbec1d41f3ddc5c91e32fa1af631
Mehak Gupta <55916817+mehakkkk@users.noreply.github.com>
2022-02-09 16:15:32 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (13.88%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,4 +1,5 @@
 class Solution {
+    //using iterative approach
     public int iterative(int num)
     {
          int count = 0;
@@ -13,8 +14,31 @@ class Solution {
         }
         return count;
     }
+    
+    //using bit manipulation
+    public int bitManipulation(int nums)
+    {
+        int count =0;
+        while(nums>0)
+        {
+            //check last bit
+            //if 0 then right shift
+            if((nums & 1) == 0)
+            {
+                nums = nums>>1;
+            }
+            else
+            {
+                nums = nums-1;
+            }
+            count++;
+        }
+        return count;
+            
+           
+    }
     public int numberOfSteps(int num) {
-       return iterative(num);
-        
+       //return iterative(num);
+        return bitManipulation(num);
     }
 }
