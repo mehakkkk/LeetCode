aabe33bfc56facee6b48cb77bd455de60ee0f2c0
Mehak Gupta <55916817+mehakkkk@users.noreply.github.com>
2023-07-13 22:25:58 +0530

    Added solution - LeetHub

---
+++ Rightmost different bit - GFG/rightmost-different-bit.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main{
+    
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		
+		//input number of testcases
+		int t=sc.nextInt();
+		int m,n;
+		while(t-->0) {
+		    
+		    
+            Solution obj = new Solution();
+            //input m and n
+		    m = sc.nextInt();
+		    n = sc.nextInt();
+		    System.out.println(obj.posOfRightMostDiffBit(m, n));
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the first position with different bits.
+    public static int posOfRightMostDiffBit(int m, int n)
+    {
+            
+        // Your code here      
+        int mask = m^n;
+        int count = 0;
+        
+        while(mask != 0)
+        {
+            count++;
+            if((mask & 1) == 1)
+            {
+                return count;
+            }
+            mask = mask>>1;
+        }
+            
+        return -1;
+    }
+}
+
+
